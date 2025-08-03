d180cc9a16beead8d79128315be5ec2b795606d6
sujalpatelcoder <sujal7455@gmail.com>
2025-08-03 23:48:56 +0530

    data types in c

---
+++ 03_data_types.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5; // Declared an integer variable named "a"
+    float b = 2.5f; // Declared a float variable named "b"
+    char c = 'C'; // Declared a char variable named "c"
+
+    return 0;
+}
