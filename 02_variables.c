dbe09147f66b458097e4b38258a2d2c52a70752f
sujalpatelcoder <sujal7455@gmail.com>
2025-08-03 23:38:41 +0530

    variables in c

---
+++ 02_variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int number = 42; // Declared a integer variable named "number"
+    float pi = 3.14f; // Declared a float variable named "pi"
+    char letter = 'A'; // Declared a char variable named "letter"
+    
+    printf("The number is: %d\n", number);
+    printf("The value of pi is: %.2f\n", pi);
+    printf("The letter is: %c\n", letter);
+    return 0;
+}
