8bbbde7d8dfa84469e6a77cd6e7fba369c908900
sujalpatelcoder <sujal7455@gmail.com>
2025-08-03 23:44:14 +0530

    rules of declaring variables

--- 02_variables.c
+++ 02_variables.c
@@ -4,6 +4,11 @@ int main() {
     int number = 42; // Declared a integer variable named "number"
     float pi = 3.14f; // Declared a float variable named "pi"
     char letter = 'A'; // Declared a char variable named "letter"
+
+    // int sujal is good; is not a valid declaration in C
+    // int $sujal or #sujal 7sujal or sujal@ is not a valid declaration in C
+    // int sujal_patel is a valid declaration in C
+    // int sujal-patel is not a valid declaration in C
     
     printf("The number is: %d\n", number);
     printf("The value of pi is: %.2f\n", pi);
