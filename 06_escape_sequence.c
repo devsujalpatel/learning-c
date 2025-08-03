d057fcb99f17e84ba7ad35fb4c6853a4f3557e55
sujalpatelcoder <sujal7455@gmail.com>
2025-08-04 00:04:59 +0530

    escape sequence character

---
+++ 06_escape_sequence.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("This is a tab:\tAnd this is a new line:\n");
+    printf("Here is a backslash: \\\n");
+    printf("Quotes: \"Double quotes\" and 'single quotes'\n");
+    return 0;
+}
