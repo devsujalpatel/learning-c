d2c408a58c2be6ded3175787ad38cd3daa02c057
sujalpatelcoder <sujal7455@gmail.com>
2025-08-04 00:52:14 +0530

    scanf example

--- 05_input.c
+++ 05_input.c
@@ -3,6 +3,13 @@
 int main() {
     int a;
     scanf("%d", &a);
+    if (a > 18) {
+        printf("You are an Adult\n");
+    }
+    else {
+        printf("You are a Minor\n");
+    }
+    
     printf("You entered: %d\n", a);
     return 0;
 }
