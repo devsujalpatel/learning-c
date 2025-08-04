72c486c649bcdd10f52fa1d492b0d69379ad9e23
sujalpatelcoder <sujal7455@gmail.com>
2025-08-04 23:30:41 +0530

    operators

--- chapter-2/02_arithmetic_operators.c
+++ chapter-2/02_arithmetic_operators.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int a = 5;
-    int b = 3;
+    int b = 2;
     // Addition operator
     int c = a + b;
     printf("The sum of %d and %d is %d\n", a, b, c);
     // Modules operator is used to get the remainder
-    printf("The remainder of %d divided by %d is %d\n", a, b, a % b);
+    int div = a % b;
+    printf("The remainder of %d divided by %d is %d\n", a, b, div);
     // Subtraction operator
     int d = a - b;
     printf("The difference between %d and %d is %d\n", a, b, d);
@@ -15,11 +17,8 @@ int main() {
     int e = a * b;
     printf("The product of %d and %d is %d\n", a, b, e);
     // Division operator
-    if (b != 0) {
-        int f = a / b;
-        printf("The quotient of %d divided by %d is %d\n", a, b, f);
-    } else {
-        printf("Division by zero is not allowed.\n");
-    }
+    float f = a / b;
+    printf("The quotient of %d divided by %d is %f\n", a, b, f);
+
     return 0;
 }
