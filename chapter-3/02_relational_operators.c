d46ee117900ec7a4bc9febe076c0a7b99f8dc977
sujalpatelcoder <sujal7455@gmail.com>
2025-08-06 22:20:10 +0530

    relational operators

---
+++ chapter-3/02_relational_operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // Relational Operators 
+
+    int n;
+    scanf("%d", &n);
+
+    // <  Check if n is greater than 10 -->
+    // > Check if n is less than 10 -->
+    // <= Check if n is equal to or greater than 10 -->
+    // >= Check if n is equal to or less than 10 -->
+    // == Check if n is equal to 10 -->
+    // != Check if n is not equal to 10 -->
+    
+
+    return 0;
+}
